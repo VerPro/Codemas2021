f87fe2395cd1b2632a00dcebeb7f1b0a52558a7e
Veronika Prokůpková <prokupkova.verca@gmail.com>
2021-12-10 15:33:57 +0100

    player and player movement added

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ let board=[
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
+let keys=[]
 
 let wall = new Image()
 wall.src = 'images/zed.png'
@@ -43,4 +44,61 @@ for (let y= 0; y <board.length; y++){
     }
 }}
 
-window.addEventListener('load', generateBoard)
+//position of the player
+let player = {
+    x: 8,
+    y: 1
+}
+
+//image of the player
+let hero = new Image
+hero.src = 'images/pernicek_dolu.png'
+
+
+function movement(){
+    //arrow right
+    if(keys['ArrowRight']){
+        hero.src='images/pernicek_doprava.png'
+        player.x++
+        
+    }
+
+    //arrow left
+    if(keys['ArrowLeft']){
+        hero.src="images/pernicek_doleva.png"
+        player.x--
+        
+    }
+
+    //arrow up
+    if(keys['ArrowUp']){
+        hero.src= 'images/pernicek_nahoru.png'
+        player.y--
+        
+    }
+
+    //arrow down
+    if(keys['ArrowDown']){
+        hero.src="images/pernicek_dolu.png"
+        player.y++
+        
+    }
+}
+function draw(){
+    ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize)
+    generateBoard()
+    movement()
+    ctx.drawImage(hero, player.x * blockSize, player.y*blockSize, blockSize, blockSize)
+}
+
+window.addEventListener('load', draw )
+document.body.addEventListener("keydown", function(e) {
+    keys[e.code] = true
+    draw()
+})
+
+document.body.addEventListener("keyup", function(e) {
+    keys[e.code] = false 
+    draw()
+})
+

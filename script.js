a945da60c1d296eca462b952eee25c226e000714
Veronika Prokůpková <prokupkova.verca@gmail.com>
2021-12-15 14:42:41 +0100

    collectible items added, counter added

--- script.js
+++ script.js
@@ -30,10 +30,43 @@ let board=[
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
-let keys=[]
+let keys = []
+let items = []
 
+//image of the walls and obstacles
 let wall = new Image()
 wall.src = 'images/zed.png'
+
+//image of the player
+let hero = new Image
+hero.src = 'images/pernicek_dolu.png'
+
+//images of the collectible objects
+let darek2 = new Image()
+darek2.src = "images/darek2.png"
+
+let darek3 = new Image()
+darek3.src = "images/darek3.png"
+
+let hulka = new Image()
+hulka.src = "images/hulka.png"
+
+let hvezda = new Image()
+hvezda.src = "images/hvezda.png"
+
+let kapr = new Image()
+kapr.src = "images/kapr.png"
+
+let ponozky = new Image()
+ponozky.src = "images/ponozky.png"
+
+let rukavice = new Image()
+rukavice.src  = 'images/rukavice.png'
+
+let game = {
+    scoreElement: document.getElementById("score"),
+    score: 0
+}
 
 function generateBoard() {
 for (let y= 0; y <board.length; y++){
@@ -42,17 +75,72 @@ for (let y= 0; y <board.length; y++){
             ctx.drawImage(wall, x*blockSize, y*blockSize, blockSize, blockSize)
         }
     }
-}}
+}
+for (let i = 0; i < items.length; i++) {
+    ctx.drawImage(
+    items[i].imageObject,
+    items[i].x * blockSize,
+    items[i].y * blockSize,
+    blockSize,
+    blockSize
+    )
+}
+}
+
+function createItems() {
+    items.push({
+        x: 1,
+        y: 1,
+        imageObject: darek2
+    })
+
+    items.push({
+        x: 1,
+        y: 15,
+        imageObject: darek3
+    })
+
+    items.push({
+        x: 14,
+        y: 12,
+        imageObject: hulka
+    })
+
+    items.push({
+        x: 15,
+        y: 18,
+        imageObject: hvezda
+    })
+
+    items.push({
+        x: 5,
+        y: 11,
+        imageObject: kapr
+    })
+
+    items.push({
+        x: 15,
+        y: 1,
+        imageObject: ponozky
+    })
+
+    items.push({
+        x: 9,
+        y: 8,
+        imageObject: rukavice
+    })
+}
+
+function startGame () {
+    createItems()
+    draw()
+}
 
 //position of the player
 let player = {
     x: 8,
     y: 1
 }
-
-//image of the player
-let hero = new Image
-hero.src = 'images/pernicek_dolu.png'
 
 function canMove(x,y){
     return (y >= 0 && y < board.length && x >= 0 && x < board[y].length && board[y][x] != 1)
@@ -87,18 +175,31 @@ function movement(){
         
     }
 }
+
+function increaseScore() {
+    game.score++
+    game.scoreElement.textContent = `${game.score}/7`
+}
 function draw(){
     ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize)
     generateBoard()
     movement()
+    collect()
     ctx.drawImage(hero, player.x * blockSize, player.y*blockSize, blockSize, blockSize)
 }
 
+function collect() {
+    for (let i = 0; i < items.length; i++) {
+        console.log(player.x + " " + items[i].x);
+        if (player.x == items[i].x && player.y == items[i].y) {
+        items.splice(i, 1)
+        increaseScore()
+        }
+    }
+}
 
 
-
-
-window.addEventListener('load', draw )
+window.addEventListener('load', startGame )
 document.body.addEventListener("keydown", function(e) {
     keys[e.code] = true
     draw()
